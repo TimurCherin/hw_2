63fbee57711b7aa500bd1d3125a8104f0259b4b9
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2023-03-28 23:03:47 +0300

    hw-2

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const str = "Привіт";
+const num = 123;
+const flag = true
+const txt = "true"
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt)
+const a = "Я";
+const b = "3";
+const c = false;
+const d = 7;
+const r = undefined;
+let t = "Введіть ім'я";
+let y = "Hello";
+let u = "77";
+let o = "false";
+let n = 33;
+const table = prompt(t)
+console.log(table)
+const age = 13;
+const friendAge = 12;
+const math = alert(age-friendAge);
+const group = 12321312;
+console.log(group,"Я навчаюся в GoITeens в групі");
+let name = "Обрано «MacBook Pro 13»";
+let price = "ціна за штуку 10000";
+console.log(name,price);
+price = "ціна за штуку 20000";
+console.log(name,price)
